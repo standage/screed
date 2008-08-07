1a8120102c01d6fa525b91790a04e209f245dab4
Alex Nolley <alex@Dimholt.(none)>
2008-08-07 15:35:28 -0400

    Updated the error-checking in the fastq writer driver file.

--- src/fqdbm.drv.cpp
+++ src/fqdbm.drv.cpp
@@ -25,6 +25,7 @@ int main(int argc, char *argv[]){
 		filepos = theFile.tellg();
 		theFile.seekg(filepos+1);
 
+		theFile >> ws;
 		// A file integrity checker
 		if(!theFile.good()){
 			cerr << "ERROR: There is something wrong with the " <<
