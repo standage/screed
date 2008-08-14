7b1402f593bd8b7d426670d7610821ff84dcc514
Alex Nolley <alex@Dimholt.(none)>
2008-08-14 11:26:01 -0400

    Added in some Windows-friendly commands which allows for proper compiling on Windows. However, the _seqdb2 module will still not compile correctly on Windows, most likely due to compiler-specific flag issues.

--- src/setup.py
+++ src/setup.py
@@ -9,10 +9,15 @@ from distutils import sysconfig
 sysconfig.get_config_vars("CFLAGS") # Set gcc's flags
 sysconfig._config_vars["CFLAGS"] = "-fno-strict-aliasing -DNDBUG -g -Wall"
 
+lib_path = os.path.join("..", "CMakeFiles", "DBREAD.dir", "src")
+if os.path.exists(os.path.join(lib_path, "dbread.lib.o")):
+  lib_path = os.path.join(lib_path, "dbread.lib.o")
+elif os.path.exists(os.path.join(lib_path, "dbread.lib.cpp.obj")):
+  lib_path = os.path.join(lib_path, "dbread.lib.cpp.obj")
 setup(
 	name = "_seqdb2",
 	ext_modules=[
-		Extension("_seqdb2", ["_seqdb2.pyx"], extra_objects=["../CMakeFiles/DBREAD.dir/src/dbread.lib.o"], pyrex_cplus=True,)
-		],
+		Extension("_seqdb2", ["_seqdb2.pyx"], extra_objects=[lib_path], pyrex_cplus=True,)
+],
 	cmdclass = {'build_ext': build_ext}
 )
