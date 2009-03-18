453ce6fa613ff4c492ac24e11853a7061a182f8a
Alex Nolley <badmit@gmail.com>
2009-03-18 15:31:58 -0400

    Added in ability to time the dbreader class with pure c++ code with the src/timing.cpp driver. Also fixed an error with the bigtests that wasn't checking for the updated .index attribute in retrieved records.

--- bigtests/__init__.py
+++ bigtests/__init__.py
@@ -171,22 +171,27 @@ class Test_s22_fastq:
         """
         testcases = {}
         testcases['HWI-EAS_4_PE-FC20GCB:2:1:492:573/2'] = {
+		'index': 0L,
                 'accuracy': 'AA7AAA3+AAAAAA.AAA.;7;AA;;;;*;<1;<<<',
                 'name' : 'HWI-EAS_4_PE-FC20GCB:2:1:492:573/2',
                 'sequence': 'ACAGCAAAATTGTGATTGAGGATGAAGAACTGCTGT'}
         testcases['HWI-EAS_4_PE-FC20GCB:2:162:131:826/2'] = {
+		'index': 1895228L,
                 'accuracy': 'AAAAAAAAAAAAAAAAAAAAAA+@6=7A<05<*15:',
                 'name': 'HWI-EAS_4_PE-FC20GCB:2:162:131:826/2',
                 'sequence': 'ATGAATACAAACAATGCGGCAGTCATAATGCCCCTC'}
         testcases['HWI-EAS_4_PE-FC20GCB:2:330:88:628/2'] = {
+		'index': 3790455L,
                 'accuracy' : 'AA;AA??A5A;;+AA?AAAA;AA;9AA.AA?????9',
                 'name': 'HWI-EAS_4_PE-FC20GCB:2:330:88:628/2',
                 'sequence': 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACAAA'}
         testcases['HWI-EAS_4_PE-FC20GCB:2:4:707:391/2'] = {
+		'index': 29999L,
                 'accuracy': 'AAAAAAAAAA@<)A*AAA6A::<@AA<>A>-8?>4<',
                 'name': 'HWI-EAS_4_PE-FC20GCB:2:4:707:391/2',
                 'sequence': 'ATTAATCTCCAGTTTCTGGCAAACATTCAGGCCATT'}
         testcases['HWI-EAS_4_PE-FC20GCB:2:36:158:208/2'] = {
+		'index': 342842L,
                 'accuracy': 'AA5?AAAAA?AAAA5?AAA5A???5A>AAA4?;.;;',
                 'name': 'HWI-EAS_4_PE-FC20GCB:2:36:158:208/2',
                 'sequence': 'TTTCCCTACAGAAGTGTCTGTACCGGTAATAAAGAA'}
@@ -303,23 +308,28 @@ class Test_s31_fastq:
         """
         testcases = {}
         testcases['HWI-EAS_4_PE-FC20GCB:3:1:71:840/1'] = {
+		'index': 0L,
                 'accuracy': 'CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC',
                 'name' : 'HWI-EAS_4_PE-FC20GCB:3:1:71:840/1',
                 'sequence': 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'}
         testcases['HWI-EAS_4_PE-FC20GCB:3:330:957:433/1'] = {
-                'accuracy': 'AAAAAAAAAAA<A?<AA<AAAAA?AAA?<:*??&::',
+                'index': 4439695L,
+		'accuracy': 'AAAAAAAAAAA<A?<AA<AAAAA?AAA?<:*??&::',
                 'name': 'HWI-EAS_4_PE-FC20GCB:3:330:957:433/1',
                 'sequence': 'CTTTGTGGAGAAGAGGGCGTGGGCAAGGCACTGATA'}
         testcases['HWI-EAS_4_PE-FC20GCB:3:166:443:410/1'] = {
-                'accuracy' : 'AAAAAAAAAAAAAAAAAAAAAAAA6<@AA959???%',
+                'index': 2219847L,
+		'accuracy' : 'AAAAAAAAAAAAAAAAAAAAAAAA6<@AA959???%',
                 'name': 'HWI-EAS_4_PE-FC20GCB:3:166:443:410/1',
                 'sequence': 'TGGCATTCGCACACATCATGATGGTGCTGACCGTAA'}
         testcases['HWI-EAS_4_PE-FC20GCB:3:1:803:878/1'] = {
-                'accuracy': '?6AAA6A<A6AA<<AA?A&A066/6:/&?&1191+0',
+                'index': 2999L,
+		'accuracy': '?6AAA6A<A6AA<<AA?A&A066/6:/&?&1191+0',
                 'name': 'HWI-EAS_4_PE-FC20GCB:3:1:803:878/1',
                 'sequence': 'AAGATGCTGTAGTGGCCGCATGTGTAATAGGCTTTA'}
         testcases['HWI-EAS_4_PE-FC20GCB:3:245:54:506/1'] = {
-                'accuracy': "AAAAAAAAAAAAAAAA>A+AAA+@AA+A>A%8*?'%",
+                'index': 3329772L,
+		'accuracy': "AAAAAAAAAAAAAAAA>A+AAA+@AA+A>A%8*?'%",
                 'name': 'HWI-EAS_4_PE-FC20GCB:3:245:54:506/1',
                 'sequence': 'CTTCGTTGCTGTTTATCAGTAACTTTTTCTGGCTAG'}
 
@@ -435,23 +445,28 @@ class Test_s42_fastq:
         """
         testcases = {}
         testcases['HWI-EAS_4_PE-FC20GCB:4:1:257:604/2'] = {
-                'accuracy': 'AAAAAAAA:4>>AAA:44>>->-&4;8+8826;66.',
+                'index': 0L,
+		'accuracy': 'AAAAAAAA:4>>AAA:44>>->-&4;8+8826;66.',
                 'name' : 'HWI-EAS_4_PE-FC20GCB:4:1:257:604/2',
                 'sequence': 'TGTGGATAGTCGCCCGTGATGGCGTCGAAGTTCCGG'}
         testcases['HWI-EAS_4_PE-FC20GCB:4:330:96:902/2'] = {
-                'accuracy': 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA??????',
+                'index': 4148632L,
+		'accuracy': 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA??????',
                 'name': 'HWI-EAS_4_PE-FC20GCB:4:330:96:902/2',
                 'sequence': 'CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC'}
         testcases['HWI-EAS_4_PE-FC20GCB:4:166:158:532/2'] = {
-                'accuracy' : 'AAAAAAA?A?AAAAAAA?A>A?A?AAAAAA?.<?-?',
+                'index': 2074316L,
+		'accuracy' : 'AAAAAAA?A?AAAAAAA?A>A?A?AAAAAA?.<?-?',
                 'name': 'HWI-EAS_4_PE-FC20GCB:4:166:158:532/2',
                 'sequence': 'ATCGCCAATGCCCAGGCCTGGTTCTCTTTAACCTAT'}
         testcases['HWI-EAS_4_PE-FC20GCB:4:1:332:634/2'] = {
-                'accuracy': '?A?AAAAAAAAA8>AAAAAA*AA?A?AA.?)<9)9?',
+                'index': 3000L,
+		'accuracy': '?A?AAAAAAAAA8>AAAAAA*AA?A?AA.?)<9)9?',
                 'name': 'HWI-EAS_4_PE-FC20GCB:4:1:332:634/2',
                 'sequence': 'ACCGTGCCAGATCAGAACCTAGTGGCGATTCCAATT'}
         testcases['HWI-EAS_4_PE-FC20GCB:4:242:843:13/2'] = {
-                'accuracy': "ABAAACA?CAAA??%A;2A;/5/&:?-*1-'11%71",
+                'index': 3111474L,
+		'accuracy': "ABAAACA?CAAA??%A;2A;/5/&:?-*1-'11%71",
                 'name': 'HWI-EAS_4_PE-FC20GCB:4:242:843:13/2',
                 'sequence': 'GTTTCTATATTCTGGCGTTAGTCGTCGCCGATAATT'}
 
@@ -567,22 +582,27 @@ class Test_po_fasta:
         """
         testcases = {}
         testcases['GENSCAN00000032971'] = {
+		'index': 0L,
                 'description': 'cdna:Genscan chromosome:PPYG2:6_qbl_hap2_random:95622:98297:1  ',
                 'name' : 'GENSCAN00000032971',
                 'sequence': 'ATGGCGCCCCGAACCCTCCTCCTGCTGCTCTCGGCGGCCCTGGCCCCGACCGAGACCTGG'}
         testcases['GENSCAN00000042282'] = {
+		'index': 53997L,
                 'description': 'cdna:Genscan chromosome:PPYG2:1:229892060:229892800:1  ',
                 'name': 'GENSCAN00000042282',
                 'sequence': 'ATGATGCCATTGCAAGGACCCTCTGCAGGGCCTCAGTCCCGAGGATGGCACACAGCCTTC'}
         testcases['GENSCAN00000051311'] = {
+		'index': 30780L,
                 'description' : 'cdna:Genscan chromosome:PPYG2:10:132962172:132962871:1  ',
                 'name': 'GENSCAN00000051311',
                 'sequence': 'ATGACCCAGCCACCTACCAGGCCGCTCTGCAGACCCCCCACGGGAGCAGCCTCTGCCCCC'}
         testcases['GENSCAN00000006030'] = {
+		'index': 1469L,
                 'description': 'cdna:Genscan chromosome:PPYG2:14_random:1765749:1766075:-1  ',
                 'name': 'GENSCAN00000006030',
                 'sequence': 'ATGTGTGGCAACAAGGGCATTTCTGCCTTCCCTGAATCAGACCACCTTTTCACATGGGTA'}
         testcases['GENSCAN00000048263'] = {
+		'index': 43029L,
                 'description': "cdna:Genscan chromosome:PPYG2:6:100388173:100485454:-1  ",
                 'name': 'GENSCAN00000048263',
                 'sequence': 'ATGTGTCCCTTTGAATATGCCGGAGAACAACAGTTGCCATGGATGTGTTCTGGGGAGCCC'}
@@ -700,6 +720,7 @@ class Test_mus_fasta:
         """
         testcases = {}
         testcases['9'] = {
+		'index': 0L,
                 'description': 'dna_rm:chromosome chromosome:NCBIM37:9:1:124076172:1',
                 'name' : '9',
                 'sequence': 'NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN'}
@@ -818,22 +839,27 @@ class Test_tri_fasta:
         """
         testcases = {}
         testcases['singleUn_100'] = {
+		'index': 0L,
                 'description': '',
                 'name' : 'singleUn_100',
                 'sequence': 'TTTAAACACGTGTCCGCGCCATTTTTTTATTTATTTACCGATCAAGTGCA'}
         testcases['singleUn_9'] = {
+		'index': 2210L,
                 'description': '',
                 'name': 'singleUn_9',
                 'sequence': 'TTTAATTTTTTTACAACTCAAAATTTTGAGTAGTGTTTTAAATAGTACAC'}
         testcases['ChLG6'] = {
+		'index': 2016L,
                 'description' : '',
                 'name': 'ChLG6',
                 'sequence': 'CAAAAAAATTCATAACTCAAAAACTAAAAGTCGTAGAGCAATGCGGTTTG'}
         testcases['singleUn_286'] = {
+		'index': 186L,
                 'description': '',
                 'name': 'singleUn_286',
                 'sequence': 'AAACTAAAACATCCTTTTCAGCATATTATTTGTTATATTTAAAAAAAAAC'}
         testcases['ChLG9'] = {
+		'index': 2019L,
                 'description': '',
                 'name': 'ChLG9',
                 'sequence': 'CTGCCGATAATATTTCCTACCAGAAATAACCAATTTATTTTACGTATTAC'}
@@ -952,22 +978,27 @@ class Test_xeno_fasta:
         """
         testcases = {}
         testcases['scaffold_20095'] = {
+		'index': 0L,
                 'description': 'dna:scaffold scaffold:JGI4.1:scaffold_20095:1:2001:1',
                 'name' : 'scaffold_20095',
                 'sequence': 'GATGAGATCACCTTTCATGCTTTTTGTATCCCTATTATCTAGAGACAACAAATCAGTTGC'}
         testcases['scaffold_1'] = {
+		'index': 19500L,
                 'description': 'dna:scaffold scaffold:JGI4.1:scaffold_1:1:7817814:1',
                 'name': 'scaffold_1',
                 'sequence': 'CCTCCCTTTTTGGCTGTCTTTTCACTGTATCATAGCCTGGCGTGAACCAAGCCTCAAAAA'}
         testcases['scaffold_271'] = {
+		'index': 19230L,
                 'description' : 'dna:scaffold scaffold:JGI4.1:scaffold_271:1:1567461:1',
                 'name': 'scaffold_271',
                 'sequence': 'CGATTTTTGCGGAAAAACGCGAGTTTTTGGTAGCCATTCCGAAAGTTGCGATTTTTTGTA'}
         testcases['scaffold_19901'] = {
+		'index': 329L,
                 'description': 'dna:scaffold scaffold:JGI4.1:scaffold_19901:1:2256:1',
                 'name': 'scaffold_19901',
                 'sequence': 'ATACCGCAAAGGTTTCTTTCTTCTCAGTGCTCCATGCTGCCTCTCTTGTTTTGCCTCCCT'}
         testcases['scaffold_95'] = {
+		'index': 19408L,
                 'description': 'dna:scaffold scaffold:JGI4.1:scaffold_95:1:2899670:1',
                 'name': 'scaffold_95',
                 'sequence': 'CCCTCCTGGTGATCCCACTTCAATCTCCCCATAGGCACACATCACTTCTAGCAGTTCACA'}
